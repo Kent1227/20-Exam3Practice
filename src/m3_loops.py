512e8b0d581672ab5e93adc2340b9a2125b815fd
smithkg <smithkg@rose-hulman.edu>
2019-01-24 10:30:02 -0500

    done

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Kent Smith.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -46,7 +46,7 @@ def main():
 def run_test_practice_problem3():
     """ Tests the   practice_problem3  function. """
     ###########################################################################
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem3  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -208,8 +208,16 @@ def practice_problem3(start, n, threshold):
       :type n:     int
       :type threshold: float
     """
+    s = []
+    k = 0
+    while len(s) < n:
+        if math.sin(k + start) + math.cos(k + start) > threshold:
+            s.append(k + start)
+        k += 1
+    return s
+
     ###########################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ###########################################################################
